8573c7706319a4315c527e5ca161e68185a7c7f0
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-28 19:43:17 +0530

    reading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,9 @@ import { StudentEntryComponent } from './student-entry/student-entry.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/student-entry/student-entry.component.ts
+++ src/app/student-entry/student-entry.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./student-entry.component.css']
 })
 export class StudentEntryComponent {
+  name=""
+  rollno=""
+  adno=""
+  mobileno=""
+  college=""
+  parentname=""
+  parentmobile=""
+  username=""
+  password=""
+
+  student=()=>
+  {
+    let data:any={
+      "name":this.name,
+      "rollno":this.rollno,
+      "adno":this.adno,
+      "mobileno":this.mobileno,
+      "college":this.college,
+      "parentname":this.parentname,
+      "parentmobile":this.parentmobile,
+      "username":this.username,
+      "password":this.password
+
+    }
+    console.log(data)
+  }
 
 }
